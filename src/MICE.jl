6b35b5a5a68449837f5d3ebffab666dc5e063cc6
Hannes <hasnep@gmail.com>
2020-10-30 17:00:57 +0000

    Fix whitespace

--- src/MICE.jl
+++ src/MICE.jl
@@ -26,13 +26,13 @@ function mice(data, max_iterations = 5)
             # Only fit the model to rows where the response variable is not missing
             response_missings = data_missings[response_variable]
             data_train = data_imputed[.!response_missings, :]
-            
+
             # The explanatory variables are all the other variables
             explanatory_variables = filter(n -> n != response_variable, names(data))
-            
+
             # Fit a linear model with an intercept and each explanatory variable
             f = term(response_variable) ~ term(1) + foldl(+, term.(explanatory_variables))
-            
+
             # Fit the model
             model = lm(f, data_train)
 
@@ -40,7 +40,8 @@ function mice(data, max_iterations = 5)
             predicted_values = predict(model, data_imputed)
 
             # Replace missing values with the new imputed values
-            data_imputed_temp[response_missings, response_variable] .=    predicted_values[response_missings]
+            data_imputed_temp[response_missings, response_variable] .=
+                predicted_values[response_missings]
         end
         data_imputed = copy(data_imputed_temp)
     end
